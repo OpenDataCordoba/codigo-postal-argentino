6b89e17cab9f0885c62fd1facdf14d3c95a044de
Gaston Avila <avila.gas@gmail.com>
2017-07-10 10:42:15 -0300

    session

--- cpa/adapter.py
+++ cpa/adapter.py
@@ -1,3 +1,12 @@
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("postgresql:///cpa")
 
 Base = declarative_base()
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+
+session = DBSession()
